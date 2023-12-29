fc535edac289bfe96997d4b8efd31b687c0c048d
Rafael <codebuilder.js@gmail.com>
2023-12-29 11:45:48 -0300

    Create an event listener that capture each button clicked

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
 function Calculator() {
-    this.methods = {
-      '+': (a, b) => a + b,
-      '-': (a, b) => a - b,
-      '*': (a, b) => a * b,
-      '/': (a, b) => {
-        return (b === 0) ? 'Cannot divide by zero' : a / b
-      },
-    };
+  this.methods = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => {
+      return (b === 0) ? 'Cannot divide by zero' : a / b
+    },
+  };
     
-    this.calculate = function(operation) {
-      let splittedNumbersAndOperator = operation.split(' '),
-          a = +splittedNumbersAndOperator[0],
-          op = splittedNumbersAndOperator[1],
-          b = +splittedNumbersAndOperator[2];
+  this.calculate = function(operation) {
+    let splittedNumbersAndOperator = operation.split(' '),
+      a = +splittedNumbersAndOperator[0],
+      op = splittedNumbersAndOperator[1],
+      b = +splittedNumbersAndOperator[2];
       
-      if(!this.methods[op] || isNaN(a) || isNaN(b)) {
-        return NaN;
-      }
-      
-      return this.methods[op](a, b);
+    if(!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
     }
+      
+    return this.methods[op](a, b);
   }
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const buttonValue = button.innerHTML || button.innerText;
+
+    console.log(buttonValue);
+  });
+});
   
-  const calculator = new Calculator;
+const calculator = new Calculator;
   
-  console.log(calculator.calculate('2 + 0'));
+console.log(calculator.calculate('2 + 0'));
